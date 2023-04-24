2f243703383cd0e535189a7bbdc550b2cf190181
vbakun1 <vitaly@eeg.powertofly.com>
2023-04-24 22:19:34 +0300

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import env from "react-dotenv";
 const App: React.FC = () => {
   const webcamRef = useRef<Webcam>(null);
   const [plantName, setPlantName] = useState<string | null>(null);
+  const [plantImage, setPlantImage] = useState<string | null>(null);
 
   const captureAndIdentifyPlant = async () => {
     const imageSrc = webcamRef.current?.getScreenshot();
     if (imageSrc) {
-      const plantName = await getPlantNameFromPhoto(imageSrc);
-      setPlantName(plantName);
+      const plant = await getPlantNameFromPhoto(imageSrc);
+      setPlantName(plant.suggestions[0].plant_name);
+      setPlantImage(plant.images[0].url)
     }
   };
 
@@ -34,14 +36,15 @@ const App: React.FC = () => {
         <button className="CaptureButton" onClick={captureAndIdentifyPlant}>
           Сфотографувати і знайти рослину
         </button>
+        {plantImage && <img className="plant-image" src={plantImage}/>}
         {plantName && <div className="PlantResult">Назва рослини: {plantName}</div>}
-        <p>Стаття про рослину:</p>
+        {plantName && <p>Стаття про рослину:</p>}
         {plantName && <iframe width="100%" height="500vh" title={plantName ?? ''} src={`https://uk.m.wikipedia.org/wiki/${plantName.split(" ")[0]}`}/>}
       </div>
   );
 };
 
-async function getPlantNameFromPhoto(imageBase64: string): Promise<string | null> {
+async function getPlantNameFromPhoto(imageBase64: string): Promise<any> {
   const apiKey : string = env.REACT_APP_PLANT_ID_KEY;
   const apiUrl = 'https://api.plant.id/v2/identify';
 
@@ -61,8 +64,8 @@ async function getPlantNameFromPhoto(imageBase64: string): Promise<string | null
 
     const data = await response.json();
     if (data && data.suggestions && data.suggestions.length > 0) {
-      const plantName = data.suggestions[0].plant_name;
-      return plantName;
+      const plant = data;
+      return plant;
     }
   } catch (error) {
     console.error('Error fetching plant data:', error);
